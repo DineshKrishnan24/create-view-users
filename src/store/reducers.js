c5878b747c576240ad86790f99d4013e949019a9
dineshkrishnanj <dineshkrishnanj@arkainsure.com>
2020-07-19 19:11:25 +0530

    Sync with mutiple tabs changes

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,22 +1,29 @@
 import * as types from "./ActionTypes";
 
-const initialState = [];
+const storageValue = localStorage.getItem("users");
+const initialState = storageValue ? JSON.parse(storageValue) : [];
 
 export default function reducers(state = initialState, action) {
   if (action.type === types.CREATE_USER) {
     const { userName, contactNumber, profilePic } = action.payload;
-    return [
+    const users = [
       ...state,
       { id: state.length + 1, userName, contactNumber, profilePic },
     ];
+    localStorage.setItem("users", JSON.stringify(users));
+    return users;
   } else if (action.type === types.EDIT_USER) {
     const { id, userName, contactNumber, profilePic } = action.payload;
-    return state.map((user) => {
+    const editedUsers = state.map((user) => {
       if (user.id === id) return { id, userName, contactNumber, profilePic };
       else return user;
     });
+    localStorage.setItem("users", JSON.stringify(editedUsers));
+    return editedUsers;
   } else if (action.type === types.DELETE_USER) {
-    return state.filter((user) => user.id !== action.payload.id);
+    const newUsers = state.filter((user) => user.id !== action.payload.id);
+    localStorage.setItem("users", JSON.stringify(newUsers));
+    return newUsers;
   } else {
     return state;
   }
